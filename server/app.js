4ab773859faabc8bdc686e81a8ce7c5689d24aa0
rush <rushastronomo0415@gmail.com>
2019-04-17 15:47:17 -0700

    first commit

--- server/app.js
+++ server/app.js
@@ -1,10 +1,16 @@
-// import files and packages up here
-
-
+// import files and packages up here    
+var SdTopSpotsData = require ("./data.json")
 // create your express server below
-var app;
-
+var express = require ("express");
+var morgan = require ("morgan");
+var app = express ();
 // add your routes and middleware below
-
+app.use (morgan("dev"));
+app.get ("/", function(req, res){
+    res.status (200).send("West Side"); 
+});
+app.get("/data",function(req, res){
+    res.status (200).send(SdTopSpotsData);
+});
 // finally export the express application
 module.exports = app;
